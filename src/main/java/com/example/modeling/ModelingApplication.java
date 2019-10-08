d657a6305e7f9599e2b02fe0bdcbd9866134a9be
linzhang <linzhang@thoughtworks.com>
2019-10-08 14:03:55 +0800

    add config of hikari

--- src/main/java/com/example/modeling/ModelingApplication.java
+++ src/main/java/com/example/modeling/ModelingApplication.java
@@ -1,13 +1,14 @@
 package com.example.modeling;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class ModelingApplication {
-
 	public static void main(String[] args) {
-		SpringApplication.run(ModelingApplication.class, args);
+	    SpringApplication.run(ModelingApplication.class, args);
 	}
 
 }
